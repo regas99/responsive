35bf640dcd3a784ff8e2ec2838dec16f4028402c
Tom Sager <tom.f.sager@gmail.com>
2014-10-24 10:49:26 -0700

    added first pass to top menu

--- local_changes/rsync.cmd
+++
@@ -1,3 +0,0 @@
-rsync -avL --progress -e "ssh -i /home/tom/security/AWS_Key.pem" \
-       /media/sf_share/washoecourts/wcourt/www.washoecourts.com/* \ 
-       ec2-user@ec2-XX-XXX-XXX-XXX.us-west-2.compute.amazonaws.com:/var/www/html/www.washoecourts.com
